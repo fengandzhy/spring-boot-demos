8b36487f435f384d692884f900c5a7d25d282bc2
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2020-03-15 12:11:32 +1300

    update spring boot jdbc

--- spring-boot-demos/spring-boot-jdbc/src/main/java/org/zhouhy/springboot/jdbc/JdbcApplication.java
+++ spring-boot-demos/spring-boot-jdbc/src/main/java/org/zhouhy/springboot/jdbc/JdbcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 注意，这里如果要是的DataSource被正确注入，
  * 1. 引入的是javax.sql.DataSource;
- * 2. 在properties 文件中找到的必须是spring.datasource.driver-class-name=com.mysql.jdbc.Driver
+ *
  * 3. 在pom 文件中 必须加入jdbc的 starter
  * */
 

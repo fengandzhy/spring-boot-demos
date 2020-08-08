18800b49583898d03aa7465ba4fe4a7714d078cb
unknown <fengandzhy@gmail.com>
2020-08-08 19:19:20 +0800

    swagger2

---
+++ springboot-G/springboot-swagger/src/main/java/org/zhouhy/springboot/controller/CustomerController.java
@@ -0,0 +1,37 @@
+package org.zhouhy.springboot.controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+import org.zhouhy.springboot.entity.UserVO;
+
+import java.util.Random;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+@Api(description = "用户接口")
+@RestController
+@RequestMapping("/user")
+public class CustomerController {
+
+    @ApiOperation("修改某条数据")
+    @GetMapping(value = "/u/{id}")
+    public UserVO findById(@PathVariable int id){
+        Random rand = new Random();
+        UserVO user=new UserVO();
+        user.setId(id);
+        String temp="temp01";
+        user.setUsername(temp);
+        user.setPassword(temp);
+        int n = rand.nextInt(2);
+        user.setSex((byte)n);
+        return user;
+    }
+
+    @ApiOperation("单个用户查询，按userid查用户信息")
+    @PostMapping(value = "/user/create", produces = APPLICATION_JSON_UTF8_VALUE, consumes = APPLICATION_JSON_UTF8_VALUE)
+    public UserVO crreteUser(@RequestBody UserVO userVO) {
+        return userVO;
+    }
+}

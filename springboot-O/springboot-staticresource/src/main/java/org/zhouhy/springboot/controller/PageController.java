8905bbce2546bdd276d572eb4bd00160f2628c7e
unknown <fengandzhy@gmail.com>
2020-10-20 15:44:58 +0800

    static resource

---
+++ springboot-O/springboot-staticresource/src/main/java/org/zhouhy/springboot/controller/PageController.java
@@ -0,0 +1,16 @@
+package org.zhouhy.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    /**
+     * 引入了themeleaf之后，在这个controller的帮助下会找到在templates下面的html文件
+     * */
+    @RequestMapping(value={"/","","/index"})
+    public String index(){
+        return "index";
+    }
+}

98ebb497511155269123df4b1a05be44702483db
unknown <fengandzhy@gmail.com>
2020-10-31 12:33:20 +1300

    spring boot mybatis

--- springboot-G/springboot-mybatis/src/main/java/org/zhouhy/springboot/controller/UserController.java
+++ springboot-G/springboot-mybatis/src/main/java/org/zhouhy/springboot/controller/UserController.java
@@ -1,16 +1,15 @@
 package org.zhouhy.springboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zhouhy.springboot.mybatis.entity.User;
 import org.zhouhy.springboot.service.UserService;
 
+import java.util.List;
 import java.util.Random;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -40,5 +39,52 @@ public class UserController {
         user.setUsername(temp);
         user.setPassword(temp);
         userService.updateUserById(user);
+    }
+
+    @RequestMapping(value="/find/{id}",method = RequestMethod.GET)
+    public void findUser(@PathVariable int id){
+        User user = this.userService.findUser(100);
+        log.info(user.toString());
+    }
+
+    @RequestMapping(value="/find",method = RequestMethod.GET)
+    public void findUserBySex(@RequestParam byte sex){
+        List<User> users = this.userService.findUser(sex);
+        log.info("查到的个数是，{}",users.size());
+    }
+
+    @RequestMapping(value="/findex",method = RequestMethod.GET)
+    public void findUsers(){
+        List<User> users = this.userService.findUserByExample();
+        log.info("查到的个数是，{}",users.size());
+    }
+
+    @RequestMapping(value="/findli",method = RequestMethod.GET)
+    public void findUsersLike(){
+        List<User> users = this.userService.findUserLike();
+        log.info("查到的个数是，{}",users.size());
+        log.info(users.toString());
+    }
+
+    @RequestMapping(value="/findin",method = RequestMethod.GET)
+    public void findUsersIn(){
+        List<User> users = this.userService.findUserIn();
+        log.info("查到的个数是，{}",users.size());
+        log.info(users.toString());
+    }
+
+    @RequestMapping(value="/findpage/{offset}/{size}",method = RequestMethod.GET)
+    public void findUserspage(@PathVariable int offset,@PathVariable int size){
+        List<User> users = this.userService.findUserPage1(offset,size);
+        log.info("查到的个数是，{}",users.size());
+        for(User user:users){
+            log.info(user.toString());
+        }
+
+        users = this.userService.findUserPage2(offset,size);
+        log.info("查到的个数是，{}",users.size());
+        for(User user:users){
+            log.info(user.toString());
+        }
     }
 }
--- springboot-G/springboot-mybatis/src/main/java/org/zhouhy/springboot/service/UserService.java
+++ springboot-G/springboot-mybatis/src/main/java/org/zhouhy/springboot/service/UserService.java
@@ -1,10 +1,15 @@
 package org.zhouhy.springboot.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zhouhy.springboot.mybatis.entity.User;
 import org.zhouhy.springboot.mybatis.mapper.UserMapper;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -19,5 +24,78 @@ public class UserService {
 
     public void updateUserById(User user){
         userMapper.updateByPrimaryKey(user);
+    }
+
+    public User findUser(Integer id){
+        User user = userMapper.selectByPrimaryKey(id);
+        return user;
+    }
+
+    public List<User> findUser(byte sex){
+        User user = new User();
+        user.setSex(sex);
+        List<User> users = userMapper.select(user);
+        return users;
+    }
+
+    /**
+     * 这里的username 和 password 与原本实体类的属性名称要一一对应 大小写也必须一致
+     * */
+    public List<User> findUserByExample(){
+        Example example = new Example(User.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("username","update100");
+        criteria.andEqualTo("password","update100");
+        List<User> users = this.userMapper.selectByExample(example);
+        return users;
+    }
+
+
+    public List<User> findUserLike(){
+        Example example = new Example(User.class);
+        example.setOrderByClause("id desc");
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andLike("username","%100%");
+        List<User> users = this.userMapper.selectByExample(example);
+        return users;
+    }
+
+    public List<User> findUserIn(){
+        Example example = new Example(User.class);
+        Example.Criteria criteria = example.createCriteria();
+        List<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        criteria.andIn("id",ids);
+        List<User> users = this.userMapper.selectByExample(example);
+        return users;
+    }
+
+    /**
+     * 这里的offset的意思是从第几个元素开始查
+     * 比方说offset=8 就过滤掉前8个sex=1的元素从第九个开始显示
+     * */
+    public List<User> findUserPage1(int offset,int size){
+        User user = new User();
+        user.setSex((byte)1);
+        int count = this.userMapper.selectCount(user);
+        log.info("总条数,{}"+count);
+        List<User> users = this.userMapper.selectByRowBounds(user,new RowBounds(offset,size));
+        return  users;
+    }
+
+    /**
+     * 这里的offset的意思是从第几个元素开始查
+     * 比方说offset=8 就过滤掉前8个sex=1的元素从第九个开始显示
+     * */
+    public List<User> findUserPage2(int offset,int size){
+        User user = new User();
+        Example example = new Example(User.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("sex",1);
+        int count = this.userMapper.selectCountByExample(example);
+        log.info("总条数,{}"+count);
+        List<User> users = this.userMapper.selectByExampleAndRowBounds(example,new RowBounds(offset,size));
+        return users;
     }
 }

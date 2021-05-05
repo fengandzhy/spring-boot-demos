558d6e20e7ef888f867848a66050c4c4a913a974
fengandzhy <fengandzhy@gmail.com>
2021-05-06 06:17:31 +1200

    spring-jpa

--- springboot/springboot-jpa/src/test/java/org/zhouhy/springboot/CommentControllerTest.java
+++ springboot/springboot-jpa/src/test/java/org/zhouhy/springboot/CommentControllerTest.java
@@ -1,5 +1,8 @@
 package org.zhouhy.springboot;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +11,19 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.zhouhy.springboot.service.CommentService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CommentControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
+    
+    @Autowired
+    private CommentService commentService;
+
+    @Rule
+    public ContiPerfRule contiPerfRule = new ContiPerfRule();
 
     @Test
     public void concurrentComment() {
@@ -28,7 +38,16 @@ public class CommentControllerTest {
                 System.out.println(result);
             }).start();
         }
+    }
+
+    @Test
+    //10个线程 执行10次
+    @PerfTest(invocations = 100,threads = 10)
+    public void test() {
+
+        commentService.postComment(1l,"Abc");
 
     }
+
 }
 

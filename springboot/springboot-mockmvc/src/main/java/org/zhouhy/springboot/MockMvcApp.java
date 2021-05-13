6cdde14efebe31705cf06fe987972d2a2c818ec3
fengandzhy <fengandzhy@gmail.com>
2021-05-13 21:40:27 +1200

    mockmvc

---
+++ springboot/springboot-mockmvc/src/main/java/org/zhouhy/springboot/MockMvcApp.java
@@ -0,0 +1,11 @@
+package org.zhouhy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMvcApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MockMvcApp.class,args);
+    }
+}
---
+++ springboot/springboot-mockmvc/src/main/java/org/zhouhy/springboot/dto/User.java
@@ -0,0 +1,10 @@
+package org.zhouhy.springboot.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer Id;
+    private String username;
+    private String password;    
+}

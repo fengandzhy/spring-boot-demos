6a3c5a0155242ae5899ea6da0fedc3ce0f1b6afa
fengandzhy <fengandzhy@gmail.com>
2021-05-20 04:54:59 +1200

    springboot-async some comments

--- springboot/springboot-async/src/main/java/org/zhouhy/springboot/AsyncApp.java
+++ springboot/springboot-async/src/main/java/org/zhouhy/springboot/AsyncApp.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 1. 这种异步线程池的需要这么以下几个步骤
- *  1) 先创建一个所有线程池的父类, 并在这个类标记上@EnableAsync
+ *  1) 先创建一个所有线程池的父类, 并在这个类标记上@EnableAsync 和 @Configuration
+ *  2) 创建线程池子类, 例如CreditTaskConfiguration
+ *  3) 在service的相应方法上加入相应的线程池@Async("redisTaskExecutor")
+ * 
+ * 
  * 
  * */
 @SpringBootApplication

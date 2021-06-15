f583ea445ebde98b67aa647401610ab36733de75
fengandzhy <fengandzhy@gmail.com>
2021-06-15 20:11:18 +1200

    update the db name

--- springboot/springboot-druid/src/main/java/org/zhouhy/springboot/DruidApp.java
+++ springboot/springboot-druid/src/main/java/org/zhouhy/springboot/DruidApp.java
@@ -1,11 +1,12 @@
 package org.zhouhy.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class DruidApp {
     public static void main(String[] args) {
-        
+        SpringApplication.run(DruidApp.class,args);
     }
 }

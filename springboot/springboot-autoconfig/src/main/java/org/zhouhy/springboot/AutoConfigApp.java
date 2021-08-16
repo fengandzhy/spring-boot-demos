00bafbc1c71b97924ed11f7949ab4d21f0e4f6d1
fengandzhy <fengandzhy@gmail.com>
2021-08-16 21:37:53 +1200

    springboot-autoconfig

--- springboot/springboot-autoconfig/src/main/java/org/zhouhy/springboot/AutoConfigApp.java
+++ springboot/springboot-autoconfig/src/main/java/org/zhouhy/springboot/AutoConfigApp.java
@@ -6,6 +6,19 @@ import org.springframework.context.annotation.Import;
 import org.zhouhy.springboot.config.ConditionalOnPropertyOrderConfig;
 import org.zhouhy.springboot.config.ConditionalOnPropertyUserConfig;
 
+/**
+ * 1. 关于这个@Import 
+ *  1) 如果该类实现了 ImportSelector 接口，Spring 容器就会实例化该类，并且调用其 selectImports 方法；
+ *  2) 如果该类实现了 DeferredImportSelector 接口，则 Spring 容器也会实例化该类并调用其 selectImports方法。
+ *  DeferredImportSelector 继承了 ImportSelector，区别在于 DeferredImportSelector 实例的 selectImports 方法调用时机晚于 ImportSelector 的实例，
+ *  要等到 @Configuration 注解中相关的业务全部都处理完了才会调用；
+ *  3) 如果该类实现了 ImportBeanDefinitionRegistrar 接口，Spring 容器就会实例化该类，并且调用其 registerBeanDefinitions 方法
+ *  
+ * 
+ * 
+ * 
+ * */
+
 @SpringBootApplication
 @Import({ ConditionalOnPropertyOrderConfig.class, ConditionalOnPropertyUserConfig.class })
 public class AutoConfigApp {
--- springboot/springboot-autoconfig/src/main/java/org/zhouhy/springboot/controller/ConditionalOnPropertyController.java
+++ springboot/springboot-autoconfig/src/main/java/org/zhouhy/springboot/controller/ConditionalOnPropertyController.java
@@ -40,27 +40,27 @@ public class ConditionalOnPropertyController {
                 "orderService=" + orderService;
     }
 
-    @GetMapping("/names")
-    public String getBeanNames(){
-        ApplicationContext applicationContext 
-                = new AnnotationConfigApplicationContext(ConditionalOnPropertyUserConfig.class);
-        String[] names = applicationContext.getBeanDefinitionNames();
-        String result = "";
-        for (String name : names) {
-            System.out.println(">>>>>>" + name);
-            result=result+name+"<br/>";
-        }
-        return result;
-    }
-
-    @GetMapping("/names1")
-    public String getBeanNames1(){
-        String[] names = applicationContext.getBeanDefinitionNames();
-        String result = "";
-        for (String name : names) {
-            System.out.println(">>>>>>" + name);
-            result=result+name+"<br/>";
-        }
-        return result;
-    }
+//    @GetMapping("/names")
+//    public String getBeanNames(){
+//        ApplicationContext applicationContext 
+//                = new AnnotationConfigApplicationContext(ConditionalOnPropertyUserConfig.class);
+//        String[] names = applicationContext.getBeanDefinitionNames();
+//        String result = "";
+//        for (String name : names) {
+//            System.out.println(">>>>>>" + name);
+//            result=result+name+"<br/>";
+//        }
+//        return result;
+//    }
+//
+//    @GetMapping("/names1")
+//    public String getBeanNames1(){
+//        String[] names = applicationContext.getBeanDefinitionNames();
+//        String result = "";
+//        for (String name : names) {
+//            System.out.println(">>>>>>" + name);
+//            result=result+name+"<br/>";
+//        }
+//        return result;
+//    }
 }

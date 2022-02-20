2de0b04945598031d7ec81fe8e545f6981be89fe
fengandzhy <fengandzhy@gmail.com>
2022-02-20 22:15:58 +1300

    bannner

---
+++ spring-boot/spring-boot-beginning/src/main/java/org/example/spring/boot/beginning/BeginningApp.java
@@ -0,0 +1,15 @@
+package org.example.spring.boot.beginning;
+
+import org.example.spring.boot.beginning.banner.UnimarketBanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginningApp {
+    public static void main(String[] args) {
+//        SpringApplication.run(BeginningApp.class,args);
+        SpringApplication springApplication = new SpringApplication(BeginningApp.class);
+        springApplication.setBanner(new UnimarketBanner());
+        springApplication.run(args);
+    }
+}
---
+++ spring-boot/spring-boot-beginning/src/main/java/org/example/spring/boot/beginning/banner/UnimarketBanner.java
@@ -0,0 +1,46 @@
+package org.example.spring.boot.beginning.banner;
+
+import org.springframework.boot.Banner;
+import org.springframework.core.env.Environment;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnimarketBanner implements Banner {   
+    
+    @Override
+    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+        List<String> banner = this.getBannerContent(); 
+        out.println();
+        for(String line:banner){
+            out.println(line);
+        }
+        out.println(); // 输出一个换行
+        out.flush(); // 强制清空缓存
+    }
+    
+    public List<String> getBannerContent() {
+        List<String> banner = new ArrayList<>();
+        URL resource = UnimarketBanner.class.getClassLoader().getResource("banner_del.txt");
+        try {
+            assert resource != null;
+            File file = new File(resource.getFile());
+            FileInputStream fis = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            BufferedReader reader = new BufferedReader(isr);
+            while(true){
+                String line = reader.readLine();
+                if(line == null){
+                    break;
+                }
+                banner.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return banner;
+    }
+}

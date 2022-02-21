d876b392034d342152d31d8b003e0531eafb8049
fengandzhy <fengandzhy@gmail.com>
2022-02-21 20:22:19 +1300

    spring-boot-configuration

---
+++ spring-boot/spring-boot-configuration/src/main/java/org/example/spring/boot/configuration/ConfigurationApp.java
@@ -0,0 +1,11 @@
+package org.example.spring.boot.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationApp.class,args);
+    }
+}

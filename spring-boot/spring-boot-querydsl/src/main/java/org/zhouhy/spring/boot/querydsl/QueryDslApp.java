d54e786e980b2846c31fae98e3779fa65b45c125
fengandzhy <fengandzhy@gmail.com>
2022-07-17 16:40:51 +1200

    spring-boot querydsl

---
+++ spring-boot/spring-boot-querydsl/src/main/java/org/zhouhy/spring/boot/querydsl/QueryDslApp.java
@@ -0,0 +1,11 @@
+package org.zhouhy.spring.boot.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslApp {
+    public static void main(String[] args) {
+        SpringApplication.run(QueryDslApp.class,args);
+    }
+}

43b72839407723749fb380afbc856a434d59c4a4
fengandzhy <fengandzhy@gmail.com>
2022-08-23 18:28:47 +1200

    spring boot beginning update the banner

--- spring-boot/spring-boot-beginning/src/main/java/org/example/spring/boot/beginning/BeginningApp.java
+++ spring-boot/spring-boot-beginning/src/main/java/org/example/spring/boot/beginning/BeginningApp.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeginningApp {
     public static void main(String[] args) {
-//        SpringApplication.run(BeginningApp.class,args);
         SpringApplication springApplication = new SpringApplication(BeginningApp.class);
         springApplication.setBanner(new UnimarketBanner());
         springApplication.run(args);

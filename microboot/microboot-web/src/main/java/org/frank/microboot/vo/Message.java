4743a2f7b9dd9d5f3a27ca375d48fdabbfbe61cd
fengandzhy <fengandzhy@gmail.com>
2022-09-04 11:18:11 +1200

    microboot lombok builder

--- microboot/microboot-web/src/main/java/org/frank/microboot/vo/Message.java
+++ microboot/microboot-web/src/main/java/org/frank/microboot/vo/Message.java
@@ -1,12 +1,19 @@
 package org.frank.microboot.vo;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import java.util.Date;
 
 @Data // 这就是Lombok注解，这个注解使用的是最频繁的
+@Builder
+//@NoArgsConstructor
 public class Message {
     private String title;
     private Date pubdate;
     private String content;
+
+    
 }

67616d80a477af99237bc22e3350af1f4b398e30
fengandzhy <fengandzhy@gmail.com>
2022-09-08 20:47:21 +1200

    spring boot parameter

---
+++ spring-boot/spring-boot-parameters/src/main/java/org/zhouhy/spring/boot/paramters/dtos/Employee.java
@@ -0,0 +1,15 @@
+package org.zhouhy.spring.boot.paramters.dtos;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Employee {
+    private Integer Id;    
+    private String username;    
+    private String password;
+    private Integer age;
+    private Date date;    
+}

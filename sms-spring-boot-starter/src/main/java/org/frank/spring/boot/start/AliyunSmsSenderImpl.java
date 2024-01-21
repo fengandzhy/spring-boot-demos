d5c4429269c27b50251d274ab5e869a77ffaa7e7
fengandzhy <fengandzhy@gmail.com>
2024-01-21 17:49:38 +1300

    update the pom file

---
+++ sms-spring-boot-starter/src/main/java/org/frank/spring/boot/start/AliyunSmsSenderImpl.java
@@ -0,0 +1,16 @@
+package org.frank.spring.boot.start;
+
+public class AliyunSmsSenderImpl implements SmsSender{
+
+    private SmsProperties.SmsMessage smsMessage;
+
+    public AliyunSmsSenderImpl(SmsProperties.SmsMessage smsProperties) {
+        this.smsMessage = smsProperties;
+    }
+
+    @Override
+    public boolean send(String message) {
+        System.out.println(smsMessage.toString()+"开始发送短信==》短信内容："+message);
+        return true;
+    }
+}

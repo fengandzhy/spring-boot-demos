10108446ab386b50124be0df957d7c749b2b8616
fengandzhy <fengandzhy@gmail.com>
2024-01-21 18:38:38 +1300

    springboot-web filter and listeners

---
+++ spring-boot/spring-boot-sms-starter/src/main/java/org/frank/spring/boot/sms/starter/AutoconfigApplication.java
@@ -0,0 +1,20 @@
+package org.frank.spring.boot.sms.starter;
+
+
+import org.frank.spring.boot.start.AliyunSmsSenderImpl;
+import org.frank.spring.boot.start.TencentSmsSenderImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@SpringBootApplication
+public class AutoconfigApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AutoconfigApplication.class, args);
+        AliyunSmsSenderImpl aliyunSmsSender = applicationContext.getBean(AliyunSmsSenderImpl.class);
+        aliyunSmsSender.send("用阿里云发送短信");
+        TencentSmsSenderImpl tencentSmsSender = applicationContext.getBean(TencentSmsSenderImpl.class);
+        tencentSmsSender.send("用腾讯云发送短信");
+    }
+}

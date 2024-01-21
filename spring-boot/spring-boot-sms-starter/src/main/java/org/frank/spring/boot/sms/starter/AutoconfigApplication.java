51f3251ec731cb5d50697357af155dee4ca1dc1f
fengandzhy <fengandzhy@gmail.com>
2024-01-21 18:54:08 +1300

    springboot-web filter and listeners

--- spring-boot/spring-boot-sms-starter/src/main/java/org/frank/spring/boot/sms/starter/AutoconfigApplication.java
+++ spring-boot/spring-boot-sms-starter/src/main/java/org/frank/spring/boot/sms/starter/AutoconfigApplication.java
@@ -2,12 +2,14 @@ package org.frank.spring.boot.sms.starter;
 
 
 import org.frank.spring.boot.start.AliyunSmsSenderImpl;
+import org.frank.spring.boot.start.EnableSms;
 import org.frank.spring.boot.start.TencentSmsSenderImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 
+@EnableSms
 @SpringBootApplication
 public class AutoconfigApplication {
     public static void main(String[] args) {

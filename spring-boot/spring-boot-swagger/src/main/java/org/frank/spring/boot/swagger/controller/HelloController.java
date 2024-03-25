f96c4191b6436ea65a4fd27bb1855ea98297616d
fengandzhy <fengandzhy@gmail.com>
2024-03-25 21:06:41 +1300

    springboot-swagger api model

--- spring-boot/spring-boot-swagger/src/main/java/org/frank/spring/boot/swagger/controller/HelloController.java
+++ spring-boot/spring-boot-swagger/src/main/java/org/frank/spring/boot/swagger/controller/HelloController.java
@@ -4,7 +4,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.frank.spring.boot.swagger.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,11 @@ public class HelloController {
     @ApiOperation("test search")
     public String search(String name, Integer age){
         return name + ":" + age;
+    }
+    
+    @PostMapping("/create")
+    @ApiOperation("test create")
+    public String createUser(User user){
+        return user.toString();
     }
 }

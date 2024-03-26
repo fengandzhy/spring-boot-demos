2e56b7abd474051068316d05f8923f5ab9c3d04f
fengandzhy <fengandzhy@gmail.com>
2024-03-26 16:27:18 +1300

    springboot-validate

---
+++ spring-boot/spring-boot-validate/src/main/java/org/frank/spring/boot/validate/controller/UserController.java
@@ -0,0 +1,39 @@
+package org.frank.spring.boot.validate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.frank.spring.boot.validate.vo.UserVO;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+@RestController
+@RequestMapping(value="/user")
+@Slf4j
+public class UserController {
+
+    @RequestMapping(value="/create",consumes = APPLICATION_JSON_UTF8_VALUE)
+    public void createUser(@RequestBody @Validated UserVO userVO){
+        log.info("creating a user....");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("done");
+    }
+
+    @RequestMapping(value="/update",consumes = APPLICATION_JSON_UTF8_VALUE)
+    public void updateUser(String userId){
+        UserVO userVO = this.getUserById(userId);
+        Assert.notNull(userVO,"用户为空");
+    }
+
+    private UserVO getUserById(String userId){
+        return null;
+    }
+}

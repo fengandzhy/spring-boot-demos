f07e08e5a4c8a0053966c7c038addfb532dc181f
fengandzhy <fengandzhy@gmail.com>
2024-03-26 16:24:58 +1300

    springboot-validate

---
+++ spring-boot/spring-boot-validate/src/main/java/org/frank/spring/boot/validate/ValidateApplication.java
@@ -0,0 +1,12 @@
+package org.frank.spring.boot.validate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidateApplication.class, args);
+    }
+}
